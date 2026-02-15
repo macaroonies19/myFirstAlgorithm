adcd564a9e00b1add55a76ce0f6f56abaac7bd54
macaroonies19 <alexalksnis@gmail.com>
2026-02-16 10:42:17 +1100

    added menu function

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+Order = 'none'
+print(Order)
+def menu():
+    Menu = ['Burger, Chocolate Milk, Garlic Bread, Juice, Pasta, Scroll']
+    Prices = [5.00, 2.50, 2.00, 2.50, 6.50]
+    print("Menu:")
+    print(Menu)
+    print(Prices)
+    Order = input('What would you like? ')
+    return menu()
 
+def order():
+    print('Added to cart')
+
+order()
+menu()
